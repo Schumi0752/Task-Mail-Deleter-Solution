7e14fa4121e6b213357a5424a897b15c3e211137
Schumi0752 <sumit@adroitnetwork.com>
2017-06-07 16:34:56 +0530

    initial check in for solution

---
+++ Task.MailDeleter/Program.cs
@@ -0,0 +1,19 @@
+﻿using Metrics360.Mail.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Task.MailDeleter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Metrics360.Mail.MetricsMailService mailService = new Metrics360.Mail.MetricsMailService();
+            mailService.DeleteMails(m=>m.Sent); 
+        }
+    }
+}
